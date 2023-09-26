15da7f234cc0b0b0ecf44dc1b50d244a651af354
Christian <munarc3@gmail.com>
2023-09-26 14:54:01 -0700

    mainApptest

--- app/src/main/java/com/example/meowtea/MainActivity.kt
+++ app/src/main/java/com/example/meowtea/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.meowtea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.meowtea.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment()
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.store -> {
+                    // Handle home item
+                }
+                R.id.cart -> {
+                    // Handle profile item
+                }
+
+            }
+            true
+        }
+
+         fun replaceFragment(fragment: Fragment) {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
