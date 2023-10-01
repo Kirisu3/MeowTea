a089639ab2cbadbf5d1dfa8b01687954603749d3
RenierRains <spideypeter04@gmail.com>
2023-10-01 13:35:01 +0800

    add data model + gradle fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 
 dependencies {
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
---
+++ app/src/main/java/com/example/meowtea/MilkTea.kt
@@ -0,0 +1,8 @@
+package com.example.meowtea
+
+data class MilkTea(
+    val name: String,
+    val description: String,
+    val price: Double,
+    val imagePath: String
+)
