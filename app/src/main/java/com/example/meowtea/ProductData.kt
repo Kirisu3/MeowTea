4a9b54ccfd41fc603269cbdeb6d1d32bb6a6fbdb
RenierRains <spideypeter04@gmail.com>
2023-10-04 13:56:47 +0800

    DB FIX

--- app/src/main/java/com/example/meowtea/ProductData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.meowtea
-data class ProductData(var img : Int,
-                       var name: String,
-                       var description: String,
-                       var price: String)
