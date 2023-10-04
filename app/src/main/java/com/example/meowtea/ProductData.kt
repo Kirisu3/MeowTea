f88af703405076a83657a32e98d4732be9cfa705
yuuki <yuuki.diola19@gmail.com>
2023-10-04 11:45:31 +0800

    RecyclerView

---
+++ app/src/main/java/com/example/meowtea/ProductData.kt
@@ -0,0 +1,5 @@
+package com.example.meowtea
+data class ProductData(var img : Int,
+                       var name: String,
+                       var description: String,
+                       var price: String)
