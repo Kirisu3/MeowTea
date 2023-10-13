0d4cbfe54300746268de5e9788719a99a13b7b37
Christian <munarc3@gmail.com>
2023-10-13 14:01:41 +0800

    top_bar ni Dwayne

--- app/src/main/java/com/example/meowtea/MainActivity.kt
+++ app/src/main/java/com/example/meowtea/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.meowtea
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
@@ -18,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     private val storeFragment = StoreFragment()
     private val cartFragment = CartFragment()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
        super.onCreate(savedInstanceState)
         Thread.sleep(3000)
         installSplashScreen()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(storeFragment)
