cf15b3876b28bc4806b6649ba7bebdb3e580cbfe
RenierRains <spideypeter04@gmail.com>
2023-10-13 23:47:31 +0800

    FIXED DB SCHEMA, NEED 2 FIX GLIDE IMAGE ETC FOR TESTING

--- app/src/main/java/com/example/meowtea/MainActivity.kt
+++ app/src/main/java/com/example/meowtea/MainActivity.kt
@@ -4,13 +4,10 @@ package com.example.meowtea
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import com.example.meowtea.database.AppDatabase
 import com.example.meowtea.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,11 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val storeFragment = StoreFragment()
     private val cartFragment = CartFragment()
-    val coroutineScope = CoroutineScope(Dispatchers.IO)
 
-    private val milkTeaDatabase: AppDatabase by lazy {
-        AppDatabase.create(this)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,21 +36,7 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val milkTeaAdapter = MilkTeaAdapter(ArrayList()) // Initialize with an empty list
 
-        val milkTeaDao = milkTeaDatabase.milkTeaDao()
-
-        coroutineScope.launch(Dispatchers.IO) {
-            // Fetch the milk teas from the database on a background thread
-            val milkTeas = milkTeaDao.getAll()
-
-            // Update the UI on the main thread with the result
-            withContext(Dispatchers.Main) {
-                milkTeaAdapter.updateData(milkTeas)
-                recyclerView.adapter = milkTeaAdapter
-            }
-        }
     }
 
 
--- app/src/main/java/com/example/meowtea/StoreFragment.kt
+++ app/src/main/java/com/example/meowtea/StoreFragment.kt
@@ -5,21 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.meowtea.database.AppDatabase
 import com.example.meowtea.databinding.FragmentStoreBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class StoreFragment : Fragment() {
     private lateinit var binding: FragmentStoreBinding
+
+    private val milkTeaDatabase: AppDatabase by lazy {
+        AppDatabase.create(this.requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentStoreBinding.inflate(inflater, container, false)
-        val view = binding.root
+        if (view == null) {
+            binding = FragmentStoreBinding.inflate(inflater, container, false)
+        }
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val milkTeaAdapter = MilkTeaAdapter(ArrayList()) // Initialize with an empty list
+        val milkTeaDao = milkTeaDatabase.milkTeaDao()
 
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            // Fetch the milk teas from the database on a background thread
+            val milkTeas = milkTeaDao.getAll()
 
+            // Update the UI on the main thread with the result
+            withContext(Dispatchers.Main) {
+                milkTeaAdapter.updateData(milkTeas)
+                recyclerView.adapter = milkTeaAdapter
+            }
+        }
 
-        return view
+        return binding.root
     }
 }
+
 
