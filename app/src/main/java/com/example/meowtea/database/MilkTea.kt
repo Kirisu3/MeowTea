ebc631aec27b32c272c39978cb3a0ca9396a171e
Christian <munarc3@gmail.com>
2023-10-18 12:29:36 +0800

    Pics

--- app/src/main/java/com/example/meowtea/database/MilkTea.kt
+++ app/src/main/java/com/example/meowtea/database/MilkTea.kt
@@ -1,42 +1,14 @@
 package com.example.meowtea.database
 
-import android.os.Parcel
-import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "MilkTeaTable")
 data class MilkTea(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    val name: String,
-    val price: Int,
-    val imagePath: String
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString() ?: "",
-        parcel.readInt(),
-        parcel.readString() ?: ""
-    )
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-        parcel.writeInt(price)
-        parcel.writeString(imagePath)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MilkTea> {
-        override fun createFromParcel(parcel: Parcel): MilkTea {
-            return MilkTea(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MilkTea?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    @PrimaryKey(autoGenerate = true) val id:  Int?,
+    @ColumnInfo(name= "name") val name: String,
+    @ColumnInfo(name= "price") val price: Double,
+    @ColumnInfo(name= "imgPath") val image: String
+)
